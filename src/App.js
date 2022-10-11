47d4d5b3f11e8e53e5a472a1c0efefe4f28adbae
我菜得睡不着觉 <jouskaaaa@gmail.com>
2022-10-11 23:13:28 +0200

    add antd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import 'antd/dist/antd.css';
+import { ColumnsType } from 'antd/es/table';
+import {Space, Table, Layout, Col} from "antd";
+import {Header, Footer, Content} from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+import Row from "antd/es/descriptions/Row";
+
+let datacloumns;
+datacloumns = [
+    {
+        title: '周期',
+        dataIndex: 'm_period',
+        key: 'm_period',
+    },
+    {
+        title: '比赛名称',
+        dataIndex: 'm_name',
+        key: 'm_name',
+    },
+    {
+        title: '赛事级别',
+        dataIndex: 'm_level',
+        key: 'm_level',
+    },
+    {
+        title: '比赛时间',
+        dataIndex: 'm_date',
+        key: 'm_date',
+    },
+    {
+        title: '比赛场次',
+        dataIndex: 'm_event',
+        key: 'm_event',
+    },
+    {
+        title: '赛果',
+        dataIndex: 'm_result',
+        key: 'm_result',
+    },
+    {
+        title: '大比分',
+        dataIndex: 'm_score_b',
+        key: 'm_score_b',
+    },
+    {
+        title: '小比分',
+        dataIndex: 'm_score_s',
+        key: 'm_score_s',
+    },
+];
 
 function req() {
     return fetch('http://127.0.0.1:5000/show_all').then(res => {
@@ -16,54 +66,33 @@ function App() {
             setLoading(true);
             req().then(
                 (matchInfo) => {
-                    console.log(matchInfo);
                     setData(matchInfo);
                     setLoading(false);
                 }
             );
         }
     });
-    const row = [];
-    if(data){
-        for (const d of data) {
-            row.push(
-                <tr key={d.id.toString()}>
-
-                    {/*m_period, m_name, m_level, m_date_s, m_date_e, m_event, m_rank, m_cate, m_date, m_progress, oppo_nation, oppo_name_en, oppo_name_cn, oppo_name_cn_f, m_result, m_score_b, m_score_s*/}
-
-                    <td key="m_period">{d.m_period}</td>
-                    <td key="m_name">{d.m_name}</td>
-                    <td key="m_level">{d.m_level}</td>
-                    <td key="m_date">{d.m_date}</td>
-                    <td key="m_event">{d.m_event}</td>
-                    <td key="m_result">{d.m_result ? '胜' : '负'}</td>
-                    <td key="m_score_b">{d.m_score_b}</td>
-                    <td key="m_score_s">{d.m_score_s}</td>
-                </tr>
-            );
-        }
-    }
-
     return (
-        <div className="App">
-            <table>
-                <thead>
-                    <tr>
-                        <th key="m_period">周期</th>
-                        <th key="m_name">比赛名称</th>
-                        <th key="m_level">赛事级别</th>
-                        <th key="m_date">比赛时间</th>
-                        <th key="m_event">比赛场次</th>
-                        <th key="m_result">赛果</th>
-                        <th key="m_score_b">大比分</th>
-                        <th key="m_score_s">小比分</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {row}
-                </tbody>
-            </table>
-        </div>
+        <Layout>
+            <Header id="main-header">
+                <div className="logo" />
+                <div>⭐️樊振东战绩查询系统⭐️</div>
+            </Header>
+            <Layout>
+                <Sider></Sider>
+                <Content>
+                    <div className="main-table">
+                        <Table columns={datacloumns} dataSource={data} />
+                    </div>
+                    {/*<Row>*/}
+                    {/*    <Col>*/}
+                    {/*        <Table columns={datacloumns} dataSource={data} />*/}
+                    {/*    </Col>*/}
+                    {/*</Row>*/}
+                </Content>
+            </Layout>
+            <Footer>foot</Footer>
+        </Layout>
     );
 }
 
