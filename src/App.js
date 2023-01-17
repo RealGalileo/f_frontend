8021e5dee72ae7c383ca697d15f490e5f088c75b
我菜得睡不着觉 <jouskaaaa@gmail.com>
2023-01-17 22:46:06 +0100

    add filter

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import './App.css';
 import { useEffect, useState } from "react";
 import 'antd/dist/antd.css';
 import { ColumnsType } from 'antd/es/table';
-import {Space, Table, Layout, Col} from "antd";
+import {Space, Table, Layout, Col, Menu, DatePicker} from "antd";
 import {Header, Footer, Content} from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
 import Row from "antd/es/descriptions/Row";
 import {apiBaseUrl} from "./config";
+
+const { RangePicker } = DatePicker;
+
+const menuItems = [{
+        label: '战绩查询',
+        key: 'record',
+    },
+    {
+        label: '数据统计',
+        key: 'statistics'
+    },
+    {
+        label: '意见反馈',
+        key: 'comment'
+    },
+];
 
 function createDataColumns(filters) {
     return [
@@ -38,9 +54,20 @@ function createDataColumns(filters) {
             key: 'm_date',
         },
         {
-            title: '比赛场次',
-            dataIndex: 'm_event',
-            key: 'm_event',
+            title: '比赛属性',
+            dataIndex: 'm_cate_',
+            key: 'm_cate_',
+            filters: filters['category'],
+            filterSearch: true,
+            onFilter: (value, record) => record['m_cate_'] === value,
+        },
+        {
+            title: '搭档',
+            dataIndex: 'partner',
+            key: 'partner',
+            filters: filters['partner'],
+            filterSearch: true,
+            onFilter: (value, record) => record['partner'] === value,
         },
         {
             title: '对手',
@@ -49,6 +76,14 @@ function createDataColumns(filters) {
             filters: filters['opponent'],
             filterSearch: true,
             onFilter: (value, record) => record['oppo_name_cn'] === value,
+        },
+        {
+            title: '对手国籍',
+            dataIndex: 'oppo_nation',
+            key: 'oppo_nation',
+            filters: filters['oppo_nation'],
+            filterSearch: true,
+            onFilter: (value, record) => record['oppo_nation'] === value,
         },
         {
             title: '赛果',
@@ -92,16 +127,22 @@ function App() {
     const [data, setData] = useState();
     const [loading, setLoading] = useState(false);
     const [dataColumns, setDataColumns] = useState(createDataColumns({}));
+    const [current, setCurrent] = useState('record');
+    const [matchInfo_, setMatchInfo_] = useState();
     useEffect(() => {
         if(!loading && !data){
             setLoading(true);
             req().then(
                 (matchInfo) => {
                     setData(matchInfo);
+                    setMatchInfo_(matchInfo);
                     setDataColumns(createDataColumns({
                         'period': newFilter(matchInfo, 'm_period'),
                         'level': newFilter(matchInfo, 'm_level'),
                         'opponent': newFilter(matchInfo, 'oppo_name_cn'),
+                        'category': newFilter(matchInfo, 'm_cate_'),
+                        'oppo_nation': newFilter(matchInfo, 'oppo_nation'),
+                        'partner': newFilter(matchInfo, 'partner'),
                     }));
                     setLoading(false);
                 }
@@ -111,13 +152,32 @@ function App() {
     return (
         <Layout>
             <Header id="main-header">
-                <div className="logo" />
-                <div>⭐️樊振东战绩查询系统⭐️</div>
+                {/*<div>⭐️樊振东战绩查询系统⭐️</div>*/}
+                <Menu theme={'dark'} selectedKeys={[current]} mode={"horizontal"} items={menuItems}/>
             </Header>
             <Layout>
-                <Sider collapsible={true}></Sider>
                 <Content>
                     <div className="main-table">
+                        <div className="timepicker">
+                            通过日期筛选：
+                            <RangePicker onChange={event => {
+                                if (event) {
+                                    const start = event[0]['_d'];
+                                    const end = event[1]['_d'];
+                                    let newMatchInfo = [];
+                                    for (let i of matchInfo_) {
+                                        //console.log(i.m_date);
+                                        //console.log(dateRange.s);
+                                        if (i.m_date_timestamp != null && i.m_date_timestamp >= start.getTime()/1000 && i.m_date_timestamp <= end.getTime()/1000) {
+                                            newMatchInfo.push(i);
+                                        }
+                                    }
+                                    setData(newMatchInfo);
+                                } else {
+                                    setData(matchInfo_);
+                                }
+                            }}/>
+                        </div>
                         <Table columns={dataColumns} dataSource={data} rowKey={'id'} />
                     </div>
                     {/*<Row>*/}
