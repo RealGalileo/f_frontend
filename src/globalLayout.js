9b893c8e0a64e5c4601e33572930da061fb3a651
我菜得睡不着觉 <jouskaaaa@gmail.com>
2024-03-15 22:09:01 +0100

    add cover and animation

--- src/globalLayout.js
+++ src/globalLayout.js
@@ -8,21 +8,25 @@ function GlobalLayout(prop) {
     const globalLayoutElement = useRef(null);
     const [imgHeight, setImgHeight] = useState(-1);
     const [titleOpacity, setTitleOpacity] = useState(0);
+    const minHeight = 128;
     return (
         <div ref={globalLayoutElement} className={"global-layout"} onScroll={(e)=>{
             let screenHeight = globalLayoutElement.current.clientHeight;
             let height = screenHeight - globalLayoutElement.current.scrollTop;
-            setImgHeight(Math.max(height, 188));
-            setTitleOpacity((screenHeight - height) / (screenHeight - 188));
+            setImgHeight(Math.max(height, minHeight));
+            setTitleOpacity((screenHeight - height) / (screenHeight - minHeight));
             console.log(globalLayoutElement.current.scrollTop);
         }}>
             <div className={"cover-container"} style={{height: imgHeight < 0 ? "100vh" : imgHeight}}>
                 <img src={cover} style={{ width: "100vw", height: imgHeight < 0 ? "100vh" : imgHeight, objectFit: "cover"}}/>
                 <p id={"cover-title"} style={{opacity: titleOpacity}}>樊振东职业生涯战绩查询系统</p>
             </div>
-            <Layout style={{ minHeight: "100vh", position: "absolute", top: 0, paddingTop: "100vh", background: "transparent"}}>
+            <Layout style={{ minHeight: "100vh", position: "absolute", top: 0, background: "transparent"}}>
                 {/*<Header id='main-header'>⭐ 樊振东职业生涯战绩查询系统 ⭐</Header>*/}
-                <GlobalMenu />
+                <div style={{height: "100vh"}}/>
+                <div style={{position: "sticky", top: minHeight + "px", zIndex: 10}}>
+                    <GlobalMenu />
+                </div>
                 <Layout>
                     <Content style={{width: "100vw"}}>
                         {prop.children}
